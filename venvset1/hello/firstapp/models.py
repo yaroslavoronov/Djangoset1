c65c5b3a9bb9bde58025dd418868a79aad2a554e
yaroslavoronov <yaromoto@mail.ru>
2021-01-18 14:59:51 +0400

    add model and db

--- venvset1/hello/firstapp/models.py
+++ venvset1/hello/firstapp/models.py
@@ -2,16 +2,16 @@ from django.db import models
 
 
 class City(models.Model):
-    Name = models.CharField(max_length=20)
+    name = models.CharField(max_length=20)
 
 
 class Shop(models.Model):
-    Name = models.CharField(max_length=20)
+    name = models.CharField(max_length=20)
     idCity = models.IntegerField()
     idStreet = models.IntegerField()
     building = models.IntegerField()
 
 
 class Street(models.Model):
-    Name = models.IntegerField()
+    name = models.IntegerField()
     idCity = models.IntegerField()
